17ac388d136fdd1817257d023aeb7e91cdeae8e6
Jacobojijo <jacobojijo2017@gmail.com>
2022-12-08 22:48:08 -0800

    slight change

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def complex_delete(my_dict, value):
-    target=[]
-    for key, key_value in my_dict.items():
-	if key_value is value:
-	    targets.append(key)
-	for x in targets:
-	    del my_dict[x]
-	return(my_dict)
+def complex_delete(a_dictionary, value):
+    myDel = []
+    for i, j in a_dictionary.items():
+        if j == value:
+            myDel.append(i)
+    for i in myDel:
+        a_dictionary.pop(i)
+    return a_dictionary
