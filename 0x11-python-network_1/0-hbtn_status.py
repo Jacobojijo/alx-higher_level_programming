70a0abde518f033195db4339c03bbe3e811288a2
Jacobojijo <jacobojijo2017@gmail.com>
2023-03-31 06:58:58 -0700

    one

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
+# Python script that fetches https://intranet.hbtn.io/status
+import urllib.request
 
-
-if __name__ == '__main__':
-    import urllib.request
-
+if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
