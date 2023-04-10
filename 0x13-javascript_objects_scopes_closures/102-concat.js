881f64bb822a2b867989d0d1c139a8e2753197e3
Jacobojijo <jacobojijo2017@gmail.com>
2023-04-10 03:00:00 -0700

    advanced

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
+const sourceA = process.argv[2];
+const sourceB = process.argv[3];
+const destination = process.argv[4];
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const text1 = fs.readFileSync(sourceA, 'utf-8').trim();
+const text2 = fs.readFileSync(sourceB, 'utf-8').trim();
+const output = `${text1}\n${text2}\n`;
+fs.writeFileSync(destination, `${output.trim()}\n`);
